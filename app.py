978cade19ea8da0502ee43c182ce54edd26cd44c
nkuma23 <napa1922@gmail.com>
2021-11-09 12:32:29 -0800

    Added a new control route

--- app.py
+++ app.py
@@ -6,10 +6,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return("<h1>Hello X1</h1>") 
 
+
+@app.route('/control')
+def control():
+    return("<h2>This is the controls page. Expected in Winter 2021</h2>")
 if __name__=="__main__":
     app.run(port=5005, debug=True)
